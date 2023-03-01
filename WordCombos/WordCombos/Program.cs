e26d6b77aa11119c897b3bfe1243898cccd01b86
CSMRCK <42943919+CSMRCK@users.noreply.github.com>
2023-03-01 21:38:12 +0200

    Add result file opening

--- WordCombos/WordCombos/Program.cs
+++ WordCombos/WordCombos/Program.cs
@@ -1,17 +1,21 @@
-﻿namespace WordCombos
+﻿using System.Diagnostics;
+
+namespace WordCombos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filePath = @"..\..\..\6letters (1).txt";
-            string[] words = File.ReadAllLines(filePath);
+            string inputFilePath = @"..\..\..\6letters (1).txt";
+            string outputFilePath = @"..\..\..\Result.txt";
+            string[] words = File.ReadAllLines(inputFilePath);
 
             var combinations = from firstPart in words
                                from secondPart in words
                                where firstPart.Length + secondPart.Length == 6
                                select new { firstPart, secondPart };
-            using (StreamWriter writer = File.CreateText(@"..\..\..\Result.txt"))
+
+            using (StreamWriter writer = File.CreateText(outputFilePath))
             {
                 foreach (var combination in combinations.Distinct())
                 {
@@ -23,6 +27,7 @@
                 }
             }
 
+            Process.Start("notepad.exe", outputFilePath);
         }
     }
 }
